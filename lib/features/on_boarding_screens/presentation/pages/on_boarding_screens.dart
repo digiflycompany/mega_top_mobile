b5a625ef7578ac9b5752ea8dd25c5093ba0fcf0a
20192726 <20192726@fue.edu.eg>
2024-01-11 13:30:42 +0200

    Cubit For OnboardingScreens

--- lib/features/on_boarding_screens/presentation/pages/on_boarding_screens.dart
+++ lib/features/on_boarding_screens/presentation/pages/on_boarding_screens.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/cubit/on_boarding_cubit.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/cubit/on_boarding_states.dart';
 import 'package:mega_top_mobile/features/on_boarding_screens/presentation/pages/on_boarding_first_screen.dart';
 import 'package:mega_top_mobile/features/on_boarding_screens/presentation/pages/on_boarding_second_screen.dart';
-
 import 'on_boarding_third_screen.dart';
 
 class OnBoardingScreens extends StatefulWidget {
@@ -13,16 +15,21 @@ class OnBoardingScreens extends StatefulWidget {
 
 class _OnBoardingScreensState extends State<OnBoardingScreens> {
   late PageController _pageController;
+  late OnboardingCubit onboardingCubit;
   int pageIndex =0;
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: 0);
+    onboardingCubit = context.read<OnboardingCubit>();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: PageView(
+     body: BlocConsumer<OnboardingCubit, OnBoardingState>(
+  listener: (context, state) {},
+  builder: (context, state) {
+    return PageView(
        reverse: true,
        controller: _pageController,
        onPageChanged: (index) {
@@ -35,7 +42,9 @@ class _OnBoardingScreensState extends State<OnBoardingScreens> {
          OnboardingSecondScreen(pageController: _pageController,index: pageIndex,),
          OnboardingThirdScreen(pageController: _pageController,index: pageIndex,),
        ],
-     ),
+     );
+  },
+),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mega_top_mobile/core/utils/app_fonts.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/cubit/on_boarding_cubit.dart';
 import 'package:mega_top_mobile/features/on_boarding_screens/presentation/pages/on_boarding_screens.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +12,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData(
-        fontFamily: FontFamilies.cairo,
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider<OnboardingCubit>(
+          create: (BuildContext context) => OnboardingCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: FontFamilies.cairo,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const OnBoardingScreens(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const OnBoardingScreens(),
     );
   }
 }
