65ee911fbbf177fa589ea596c02083d46b822df8
20192726 <20192726@fue.edu.eg>
2024-01-11 11:52:24 +0200

    Onboarding Page 3 Created and its header

---
+++ lib/features/on_boarding_screens/presentation/pages/on_boarding_third_screen.dart
@@ -0,0 +1,70 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:mega_top_mobile/core/utils/app_color.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/core/utils/media_query.dart';
+import 'package:mega_top_mobile/core/utils/spacer.dart';
+import 'package:mega_top_mobile/core/widgets/primary_button.dart';
+import 'package:mega_top_mobile/core/widgets/primary_empty_button.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/presentation/widgets/header_onboarding_2.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/presentation/widgets/onboarding_description.dart';
+
+class OnboardingThirdScreen extends StatelessWidget {
+  final int index;
+  final  PageController pageController;
+  const OnboardingThirdScreen({super.key,required this.pageController, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SecondHeader(pageController: pageController,),
+            VerticalSpace(context.height*0.0346),
+            const OnboardingDescription(
+              title:AppStrings.addYourProductsToCart,
+              description: AppStrings.onBoardingDescription,
+            ),
+            VerticalSpace(context.height*0.028),
+            DotsIndicator(
+              dotsCount: 3,
+              position: index,
+              decorator: DotsDecorator(
+                size: const Size.square(9.0),
+                activeColor: AppColors.primaryColor,
+                color: AppColors.dotsColor,
+                activeSize:  Size(context.width*0.11, context.height*0.011),
+                activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              ),
+            ),
+            VerticalSpace(context.height*0.05),
+            PrimaryButton(
+                text: AppStrings.next,
+                onTap: (){
+                  pageController.animateToPage(
+                    2,
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.easeInOut,
+                  );
+                }),
+            VerticalSpace(context.height*0.0329),
+            PrimaryEmptyButton(
+                text: AppStrings.previous,
+                onTap: (){
+                  pageController.animateToPage(
+                    0,
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.easeInOut,
+                  );
+                }),
+            VerticalSpace(context.height*0.04),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/on_boarding_screens/presentation/widgets/header_onboarding_3.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mega_top_mobile/core/utils/media_query.dart';
+import 'package:mega_top_mobile/core/widgets/back_arrow_icon.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/presentation/widgets/curved_container.dart';
+import 'package:mega_top_mobile/features/on_boarding_screens/presentation/widgets/skip_text.dart';
+import '../../../../core/utils/app_assets.dart';
+
+class ThirdHeader extends StatelessWidget {
+  final PageController pageController;
+  const ThirdHeader({super.key, required this.pageController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        /// BackGroundImage ///
+        const CurvedContainer(),
+        Padding(
+          padding:  EdgeInsets.only(top:context.height*0.1),
+          child: Transform.scale(
+              scale: 0.7,
+              child: Image.asset(AppAssets.boardingCart)),
+        ),
+        Align(
+          alignment: AlignmentDirectional.centerEnd,
+          child: Padding(
+            padding:  EdgeInsets.only(top: context.width*0.12,right: context.width*0.045),
+            child: const BackArrow(),
+          ),
+        ),
+        /// Skip Text ///
+        SkipText(
+          onTap: (){},
+        ),
+      ],
+    );
+  }
+}
