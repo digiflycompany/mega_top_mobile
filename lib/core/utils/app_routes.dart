80b79a6d24c2f1960239b38ed723394cc8dde134
20192726 <20192726@fue.edu.eg>
2024-01-15 10:51:49 +0200

    app routes

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'app_color.dart';
+
+enum PageRouteAnimation { fade, scale, rotate, slide, slideBottomTop }
+
+class Routes {
+  Routes._internal();
+  static const String splashRoute = "/";
+  static const String onBoardingRoute = "/onBoardingCustom";
+  static const String onboarding1 = "/onboarding_1";
+}
+
+
+class RouteGenerator {
+  RouteGenerator._internal();
+  PageRouteAnimation? pageRouteAnimationGlobal;
+  static Duration pageRouteTransitionDurationGlobal = 300.milliseconds;
+
+
+  static Route<dynamic>? getRoute(RouteSettings routeSettings) {
+    debugPrint(routeSettings.name);
+    switch (routeSettings.name) {
+    }
+    return buildPageRoute(
+        child: const Center(
+          child: Text("no route found"),
+        ),
+        routeSettings: routeSettings);
+  }
+
+  static Route<T> buildPageRoute<T>({
+    required Widget child,
+    PageRouteAnimation? pageRouteAnimation,
+    Duration? duration,
+    RouteSettings? routeSettings,
+  }) {
+    if (pageRouteAnimation != null) {
+      if (pageRouteAnimation == PageRouteAnimation.fade) {
+        return PageRouteBuilder(
+          settings: routeSettings,
+          transitionDuration: duration ?? pageRouteTransitionDurationGlobal,
+          reverseTransitionDuration: const Duration(milliseconds: 300),
+          pageBuilder: (c, a1, a2) => child,
+          transitionsBuilder: (c, anim, a2, child) {
+            return FadeTransition(opacity: anim, child: child);
+          },
+        );
+      } else if (pageRouteAnimation == PageRouteAnimation.rotate) {
+        return PageRouteBuilder(
+          settings: routeSettings,
+          pageBuilder: (c, a1, a2) => child,
+          transitionsBuilder: (c, anim, a2, child) {
+            return RotationTransition(turns: ReverseAnimation(anim), child: child);
+          },
+          transitionDuration: duration ?? pageRouteTransitionDurationGlobal,
+        );
+      } else if (pageRouteAnimation == PageRouteAnimation.scale) {
+        return PageRouteBuilder(
+          settings: routeSettings,
+          pageBuilder: (c, a1, a2) => child,
+          transitionsBuilder: (c, anim, a2, child) {
+            return ScaleTransition(scale: anim, child: child);
+          },
+          transitionDuration: duration ?? pageRouteTransitionDurationGlobal,
+        );
+      } else if (pageRouteAnimation == PageRouteAnimation.slide) {
+        return PageRouteBuilder(
+          settings: routeSettings,
+          pageBuilder: (c, a1, a2) => child,
+          transitionsBuilder: (c, anim, a2, child) {
+            return SlideTransition(
+              position: Tween(
+                begin: const Offset(1.0, 0.0),
+                end: const Offset(0.0, 0.0),
+              ).animate(anim),
+              child: child,
+            );
+          },
+          transitionDuration: duration ?? pageRouteTransitionDurationGlobal,
+        );
+      } else if (pageRouteAnimation == PageRouteAnimation.slideBottomTop) {
+        return PageRouteBuilder(
+          settings: routeSettings,
+          pageBuilder: (c, a1, a2) => child,
+          transitionsBuilder: (c, anim, a2, child) {
+            return SlideTransition(
+              position: Tween(
+                begin: const Offset(0.0, 1.0),
+                end: const Offset(0.0, 0.0),
+              ).animate(anim),
+              child: child,
+            );
+          },
+          transitionDuration: duration ?? pageRouteTransitionDurationGlobal,
+        );
+      }
+    }
+    return MaterialPageRoute<T>(
+      builder: (_) => AnnotatedRegion<SystemUiOverlayStyle>(
+          value: const SystemUiOverlayStyle(
+            statusBarColor: AppColors.transparent,
+            statusBarIconBrightness: Brightness.dark,
+            statusBarBrightness: Brightness.dark,
+          ),
+          child: child),
+      settings: routeSettings,
+    );
+  }
+}
