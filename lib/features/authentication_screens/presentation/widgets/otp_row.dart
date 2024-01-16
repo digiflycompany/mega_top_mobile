855ab120ff7aa73151f90dea699a5680d6791e4d
20192726 <20192726@fue.edu.eg>
2024-01-16 17:52:06 +0200

    Custom Otp boxes Row

---
+++ lib/features/authentication_screens/presentation/widgets/otp_row.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'otp_box.dart';
+
+class OtpRow extends StatelessWidget {
+  const OtpRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: context.width*0.11),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          OtpBox(number: AppStrings.four,),
+          OtpBox(number: AppStrings.five,),
+          OtpBox(number: AppStrings.six,),
+          OtpBox(number: AppStrings.seven,),
+        ],
+      ),
+    );
+  }
+}
