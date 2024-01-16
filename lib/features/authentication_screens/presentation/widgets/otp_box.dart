3895415107b51fa2cbb187cab2f903f10f73968a
20192726 <20192726@fue.edu.eg>
2024-01-16 17:44:42 +0200

    custom otp box

---
+++ lib/features/authentication_screens/presentation/widgets/otp_box.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_color.dart';
+
+class OtpBox extends StatelessWidget {
+  final String? number;
+  const OtpBox({super.key, this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      width: context.width*0.134,
+      height: context.height*0.065,
+      decoration:  BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: AppColors.otpBoxColor,
+          border: Border.all(color: AppColors.otpBoxBorderColor,width: context.width*0.002)
+      ),
+      child:  Center(
+        child: Text(
+          number!,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight:FontWeight.w400
+          ),
+        ),
+      ),
+    );
+  }
+}
