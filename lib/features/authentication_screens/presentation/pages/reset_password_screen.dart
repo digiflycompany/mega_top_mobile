30efaf4dd9046fa5a9cf3c90352d180d9bde8f9f
20192726 <20192726@fue.edu.eg>
2024-01-16 14:37:30 +0200

    Custom App Bar edited

--- lib/features/authentication_screens/presentation/pages/reset_password_screen.dart
+++ lib/features/authentication_screens/presentation/pages/reset_password_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_assets.dart';
+import 'package:mega_top_mobile/core/utils/app_color.dart';
 import 'package:mega_top_mobile/core/utils/spacer.dart';
 import 'package:mega_top_mobile/core/widgets/custom_app_bar.dart';
 import '../../../../core/utils/app_string.dart';
@@ -14,17 +17,64 @@ class ResetPasswordScreen extends StatelessWidget {
       appBar: PreferredSize(
           preferredSize: Size(double.infinity, context.height*0.087),
           child: const CustomAppBar(AppStrings.resetPasswordEn)),
-      body: Column(
-        children: [
-          VerticalSpace(context.height*0.055),
-          Row(
-            children: [
-              Container(
-
-              )
-            ],
-          )
-        ],
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: context.width*0.045),
+        child: Column(
+          children: [
+            VerticalSpace(context.height*0.055),
+            Row(
+              children: [
+                Container(
+                width: context.width*0.176,
+                height: context.height*0.087,
+                decoration: const BoxDecoration(
+                  color: AppColors.primaryColor,
+                  shape: BoxShape.circle,
+                ),
+                  child: Transform.scale(
+                      scale: 0.55,
+                      child: SvgPicture.asset(AppAssets.forgetPasswordWhiteIcon,width: 32,height: 32,)),
+                ),
+                Expanded(
+                  child: Container(
+                    color: AppColors.iconsBackgroundColor,
+                    height: context.height*0.006,
+                    width: 60,
+                  ),
+                ),
+                Container(
+                  width: context.width*0.176,
+                  height: context.height*0.087,
+                  decoration: const BoxDecoration(
+                    color: AppColors.primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Transform.scale(
+                      scale: 0.55,
+                      child: SvgPicture.asset(AppAssets.forgetPasswordWhiteIcon,width: 32,height: 32,)),
+                ),
+                Expanded(
+                  child: Container(
+                    color: AppColors.iconsBackgroundColor,
+                    height: context.height*0.006,
+                    width: 60,
+                  ),
+                ),
+                Container(
+                  width: context.width*0.176,
+                  height: context.height*0.087,
+                  decoration: const BoxDecoration(
+                    color: AppColors.primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Transform.scale(
+                      scale: 0.55,
+                      child: SvgPicture.asset(AppAssets.forgetPasswordWhiteIcon,width: 32,height: 32,)),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/authentication_screens/presentation/widgets/forget_password_custom_icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ForgotPasswordIcon extends StatelessWidget {
+  const ForgotPasswordIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
