2b7c492ff37daf186386e3e818ab959bbf4aedd9
20192726 <20192726@fue.edu.eg>
2024-01-16 11:36:26 +0200

    Custom App Bar

---
+++ lib/core/widgets/title_app_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TitleText extends StatelessWidget {
+  final   String? text;
+  const TitleText({super.key,required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text!,
+      style: const TextStyle(
+          color: Colors.black,
+          fontSize: 12,
+          fontWeight: FontWeight.w700
+      ),
+    );
+  }
+}
