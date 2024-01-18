5e5a2c37d7948b74a8b64f21e3f8f54aed337083
20192726 <20192726@fue.edu.eg>
2024-01-18 17:30:46 +0200

    Advertisment List with dots indicator done

---
+++ lib/features/home_screens/presentation/widgets/main_page_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../core/widgets/custom_search_bar.dart';
+import 'customer_information.dart';
+
+class MainPageAppBar extends StatelessWidget {
+  const MainPageAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: context.width*0.045),
+          child:  const SingleChildScrollView(
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    CustomerInformation(),
+                    CustomSearchBar(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
