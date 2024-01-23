75725c00b2d946b807d1519aa85c2ec464f21a44
20192726 <20192726@fue.edu.eg>
2024-01-23 11:43:19 +0200

    Primary Search Bar

--- lib/features/home_screens/presentation/pages/search_screen.dart
+++ lib/features/home_screens/presentation/pages/search_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/spacer.dart';
 import '../../../../core/utils/app_string.dart';
+import '../../../../core/widgets/custom_search_bar.dart';
 import '../widgets/primary_app_bar.dart';
+import '../widgets/search_bar.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -13,11 +16,15 @@ class SearchPage extends StatelessWidget {
       appBar: PreferredSize(
           preferredSize: Size(double.infinity, context.height*0.087),
           child: const PrimaryAppBar(AppStrings.searchForProductsEn)),
-      body: const Center(
-        child: Text(
-          'Search Page'
+      body:Padding(
+        padding:  EdgeInsets.symmetric(horizontal: context.width*0.045),
+        child: Column(
+          children: [
+             VerticalSpace(context.height*0.023),
+             const PrimarySearchBar(),
+          ],
         ),
-      ),
+      )
     );
   }
 }
