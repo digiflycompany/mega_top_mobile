56aa3f7ddd3f3619213419a820fb1ed08eb3f227
20192726 <20192726@fue.edu.eg>
2024-01-23 14:16:03 +0200

    Filter Container

---
+++ lib/features/home_screens/presentation/widgets/filter_box.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import '../../../../core/utils/app_assets.dart';
+import '../../../../core/utils/app_color.dart';
+
+class FilterContainer extends StatelessWidget {
+  final Function()? onTap;
+  const FilterContainer({super.key, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: context.width*0.145,
+        height: double.infinity,
+        decoration:  BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.only(
+            topRight:Radius.circular(context.height*0.006),
+            bottomRight:Radius.circular(context.height*0.006),
+          ),
+        ),
+        child: Padding(
+          padding:  EdgeInsets.symmetric(horizontal:context.width*0.045),
+          child: SvgPicture.asset(AppAssets.filterWhiteIcon),
+        ),
+      ),
+    );
+  }
+}
