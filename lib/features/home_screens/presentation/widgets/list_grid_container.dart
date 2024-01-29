f0d21f786028eebb38fb872a42b858a4c54d4c6d
20192726 <20192726@fue.edu.eg>
2024-01-29 10:25:00 +0200

    List Grid Container Clickable

--- lib/features/home_screens/presentation/widgets/list_grid_container.dart
+++ lib/features/home_screens/presentation/widgets/list_grid_container.dart
@@ -11,27 +11,30 @@ class ListGridContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height:context.height*0.055,
-      width: context.width*0.22,
-      decoration:  BoxDecoration(
-          color: AppColors.circleAvatarBackground,
-          borderRadius: BorderRadius.circular(context.height*0.006 )
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(image!),
-          HorizontalSpace(context.width*0.022 ),
-          Text(
-            text!,
-            style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w700,
-                fontSize: 15.5
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        height:context.height*0.055,
+        width: context.width*0.22,
+        decoration:  BoxDecoration(
+            color: AppColors.circleAvatarBackground,
+            borderRadius: BorderRadius.circular(context.height*0.006 )
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(image!),
+            HorizontalSpace(context.width*0.022 ),
+            Text(
+              text!,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 15.5
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
