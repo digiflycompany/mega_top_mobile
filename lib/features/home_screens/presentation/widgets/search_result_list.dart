8f88c10545dcefdf1cfa099abfc4d0e021f227a7
20192726 <20192726@fue.edu.eg>
2024-01-29 10:52:26 +0200

    Toggle list grid container

--- lib/features/home_screens/presentation/widgets/search_result_list.dart
+++ lib/features/home_screens/presentation/widgets/search_result_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:mega_top_mobile/features/home_screens/cubit/home_cubit.dart';
+import 'package:mega_top_mobile/features/home_screens/cubit/home_states.dart';
 import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/app_string.dart';
 import '../../../../core/utils/spacer.dart';
@@ -11,24 +14,34 @@ class SearchResultList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    HomeCubit homeCubit = context.read<HomeCubit>();
     return Expanded(
       child: Container(
         width: double.infinity,
         color: Colors.white,
         child: Column(
           children: [
-            VerticalSpace(context.height*0.028),
+            VerticalSpace(context.height * 0.028),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal:context.width*0.045 ),
+              padding: EdgeInsets.symmetric(horizontal: context.width * 0.045),
               child: Row(
                 children: [
-                  const ListGridContainer(
-                    image: AppAssets.listIcon,
-                    text: AppStrings.listEn
-                    ,),
+                  /// List Grid Container ///
+                  BlocConsumer<HomeCubit, HomeState>(
+                  listener: (context, state) {},
+                  builder: (context, state) {
+                    return ListGridContainer(
+                      onTap: (){
+                        homeCubit.toggleList();
+                      },
+                      image:homeCubit.isGrid? AppAssets.listIcon:AppAssets.gridIcon,
+                      text: homeCubit.isGrid? AppStrings.listEn:AppStrings.gridEn
+                      ,);
+                  },
+                                    ),
                   const Spacer(),
                   const FilterSortContainer(icon: AppAssets.sortIcon,),
-                  HorizontalSpace(context.width*0.022 ),
+                  HorizontalSpace(context.width * 0.022),
                   const FilterSortContainer(icon: AppAssets.filterIcon,)
                 ],
               ),
