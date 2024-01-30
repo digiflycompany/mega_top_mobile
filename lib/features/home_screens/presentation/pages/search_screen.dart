6d346d2c2530c32d3007a5e5b7f66271993ddab4
20192726 <20192726@fue.edu.eg>
2024-01-30 15:02:08 +0200

    No Result Column

--- lib/features/home_screens/presentation/pages/search_screen.dart
+++ lib/features/home_screens/presentation/pages/search_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:mega_top_mobile/core/utils/app_assets.dart';
 import 'package:mega_top_mobile/core/utils/spacer.dart';
+import 'package:mega_top_mobile/features/home_screens/cubit/home_cubit.dart';
+import 'package:mega_top_mobile/features/home_screens/cubit/home_states.dart';
 import 'package:mega_top_mobile/features/home_screens/presentation/widgets/latest_search_list.dart';
+import 'package:mega_top_mobile/features/home_screens/presentation/widgets/no_result_column.dart';
 import 'package:mega_top_mobile/features/home_screens/presentation/widgets/your_latest_search_row.dart';
 import '../../../../core/utils/app_color.dart';
 import '../../../../core/utils/app_string.dart';
@@ -14,69 +18,31 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    HomeCubit homeCubit = context.read<HomeCubit>();
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar:  PreferredSize(
-          preferredSize: Size(double.infinity, context.height*0.089),
-          child: const PrimaryAppBar(AppStrings.searchForProductsEn)),
-      body:Padding(
-        padding:  EdgeInsets.symmetric(horizontal: context.width*0.045),
-        child: Column(
-          children: [
-             VerticalSpace(context.height*0.012),
-             const PrimarySearchBar(),
-             // VerticalSpace(context.height*0.03),
-             VerticalSpace(context.height*0.165),
-             Expanded(
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.start,
-                 children: [
-                   Container(
-                     width: context.width*0.5,
-                     height:context.height*0.207,
-                     decoration: const BoxDecoration(
-                       color: Colors.white,
-                       shape: BoxShape.circle,
-                       boxShadow: [
-                         BoxShadow(
-                           color: AppColors.circleShadow,
-                           offset: Offset(0, 0),
-                           blurRadius: 15,
-                           spreadRadius: 0,
-                         ),
-                       ],
-                     ),
-                     child: Center(
-                       child: Image.asset(AppAssets.noResultImg,width: context.width*0.222 ,),
-                     )),
-                   VerticalSpace(context.height*0.05),
-                   const Text(
-                     AppStrings.noResultsEn,
-                     style: TextStyle(
-                       color: Colors.black,
-                       fontSize: 16,
-                       fontWeight: FontWeight.w700
-                     ),
-                   ),
-                   VerticalSpace(context.height*0.01),
-                   const Text(
-                     AppStrings.noResultLongEn,
-                     textAlign: TextAlign.center,
-                     style: TextStyle(
-                         color: AppColors.greyTextColor,
-                         fontSize: 14,
-                         fontWeight: FontWeight.w500
-                     ),
-                   ),
-                 ],
-               ),
-             )
-             // const YourLatestSearch(),
-             // VerticalSpace(context.height*0.025),
-             // const LatestSearchList(),
-          ],
-        ),
-      )
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+            preferredSize: Size(double.infinity, context.height * 0.089),
+            child: const PrimaryAppBar(AppStrings.searchForProductsEn)),
+        body: BlocConsumer<HomeCubit, HomeState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.width * 0.045),
+              child: Column(
+                children: [
+                  VerticalSpace(context.height * 0.012),
+                  const PrimarySearchBar(),
+                  homeCubit.noResult?VerticalSpace(context.height * 0.165):VerticalSpace(context.height*0.03),
+                  const NoResultColumn()
+                  // const YourLatestSearch(),
+                  // VerticalSpace(context.height*0.025),
+                  // const LatestSearchList(),
+                ],
+              ),
+            );
+          },
+        )
     );
   }
 }
---
+++ lib/features/home_screens/presentation/widgets/no_result_column.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../core/utils/app_assets.dart';
+import '../../../../core/utils/app_color.dart';
+import '../../../../core/utils/app_string.dart';
+import '../../../../core/utils/spacer.dart';
+
+class NoResultColumn extends StatelessWidget {
+  const NoResultColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+              width: context.width * 0.5,
+              height: context.height * 0.207,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.circleShadow,
+                    offset: Offset(0, 0),
+                    blurRadius: 15,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Image.asset(AppAssets.noResultImg,
+                  width: context.width * 0.222,),
+              )),
+          VerticalSpace(context.height * 0.05),
+          const Text(
+            AppStrings.noResultsEn,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w700
+            ),
+          ),
+          VerticalSpace(context.height * 0.01),
+          const Text(
+            AppStrings.noResultLongEn,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: AppColors.greyTextColor,
+                fontSize: 14,
+                fontWeight: FontWeight.w500
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
