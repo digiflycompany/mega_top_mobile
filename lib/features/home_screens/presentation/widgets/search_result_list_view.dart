0913f2d3df007dfde30e7aa505015db22e3c43be
20192726 <20192726@fue.edu.eg>
2024-01-30 13:48:18 +0200

    Search Result List View

--- lib/features/home_screens/presentation/widgets/search_result_list_view.dart
+++ lib/features/home_screens/presentation/widgets/search_result_list_view.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mega_top_mobile/features/home_screens/presentation/widgets/items_list.dart';
 import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/app_string.dart';
 import '../../data/product_model.dart';
-import 'items_grid.dart';
 
-class SearchResultGridView extends StatelessWidget {
-  const SearchResultGridView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -61,20 +61,14 @@ class SearchResultGridView extends StatelessWidget {
       ),
     ];
     return Expanded(
-      child: GridView.builder(
-         physics: const BouncingScrollPhysics(),
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: context.width * 0.027,
-          mainAxisSpacing: context.height * 0.012,
-          childAspectRatio: 0.68,
-        ),
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: products.length, // Set the item count to the length of the products list
         itemBuilder: (BuildContext context, int index) {
           final product = products[index];
-          return  Padding(
-            padding: EdgeInsets.symmetric(horizontal: context.width*0.011,vertical: context.height*0.006),
-            child: ProductsGridContainer(
+          return Padding(
+            padding: EdgeInsets.only(right: context.width*0.011,left: context.width*0.011, bottom: context.height*0.027,top: context.height*0.006),
+            child: ProductsListContainer(
               productName: product.productName,
               productPhoto: product.productPhoto,
               productType: product.productType,
