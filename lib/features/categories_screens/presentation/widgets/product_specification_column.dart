d7a8d7d54ae988d9986c1a37b49cc40ba3e8899d
20192726 <20192726@fue.edu.eg>
2024-02-07 17:04:50 +0200

    Product Specifcation Column And Row

---
+++ lib/features/categories_screens/presentation/widgets/product_specification_column.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/features/categories_screens/presentation/widgets/product_specifications_row.dart';
+import '../../../../core/utils/app_color.dart';
+import '../../../../core/utils/spacer.dart';
+
+class ProductSpecificationColumn extends StatelessWidget {
+  const ProductSpecificationColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Align(
+          alignment: AlignmentDirectional.topStart,
+          child: Text(
+            AppStrings.productSpecificationEn,
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontSize: 16
+            ),
+          ),
+        ),
+        VerticalSpace(context.height*0.021),
+        Divider(thickness: context.width*0.003,color: AppColors.smallContainerGreyColor,),
+        VerticalSpace(context.height*0.021),
+        const ProductSpecificationsRow(
+          specification: AppStrings.mainInputVoltageEn,
+          value: AppStrings.volts230En,
+        ),
+        VerticalSpace(context.height*0.021),
+        const ProductSpecificationsRow(
+          specification: AppStrings.mainOutputVoltageEn,
+          value: AppStrings.volts230En,
+        ),
+        VerticalSpace(context.height*0.021),
+        const ProductSpecificationsRow(
+          specification: AppStrings.batteryRechargingTimeEn,
+          value: AppStrings.volts230En,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/categories_screens/presentation/widgets/product_specifications_row.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../core/utils/spacer.dart';
+
+class ProductSpecificationsRow extends StatelessWidget {
+  final String? specification;
+  final String? value;
+  const ProductSpecificationsRow({super.key, this.specification, this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+         Text(
+          specification!,
+          style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w700,
+              fontSize: 14
+          ),
+        ),
+         HorizontalSpace(context.width*0.02),
+         Text(
+          value!,
+          style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 14
+          ),
+        ),
+      ],
+    );
+  }
+}
