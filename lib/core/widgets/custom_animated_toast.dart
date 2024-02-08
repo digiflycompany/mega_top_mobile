b2bf4cf776adf2e3326f5896eb4aa915a4c45691
20192726 <20192726@fue.edu.eg>
2024-02-08 18:39:01 +0200

    Custom Animated Toast Done

---
+++ lib/core/widgets/custom_animated_toast.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_color.dart';
+
+class AnimatedOverlayToast extends StatefulWidget {
+  final Color color;
+  final String message;
+  final VoidCallback onDismissed;
+
+  const AnimatedOverlayToast({
+    Key? key,
+    required this.message,
+    required this.onDismissed, required this.color,
+  }) : super(key: key);
+
+  @override
+  _AnimatedOverlayToastState createState() => _AnimatedOverlayToastState();
+}
+
+class _AnimatedOverlayToastState extends State<AnimatedOverlayToast>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 350),
+    );
+    _opacity = Tween<double>(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller.forward().then((value) {
+      Future.delayed(const Duration(seconds: 1), () {
+        _controller.reverse().then((value) {
+          widget.onDismissed();
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: _opacity.value,
+      child: Container(
+        alignment: Alignment.center,
+        height: context.height*0.087,
+        color: widget.color,
+        child: Text(
+          widget.message,
+          style:  TextStyle(
+            color: Colors.white,
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/categories_screens/presentation/widgets/favour_compare_column.dart
+++ lib/features/categories_screens/presentation/widgets/favour_compare_column.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_color.dart';
 import 'package:mega_top_mobile/features/categories_screens/presentation/widgets/white_box_icon.dart';
 import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/spacer.dart';
+import '../../../../core/widgets/custom_animated_toast.dart';
 
 class FavourCompareColumn extends StatelessWidget {
   const FavourCompareColumn({super.key});
+
+  void _showOverlay(BuildContext context) {
+    OverlayEntry? overlayEntry;
+    overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: kToolbarHeight + MediaQuery.of(context).padding.top,
+        width: MediaQuery.of(context).size.width,
+        child: AnimatedOverlayToast(
+          message: 'The product added to wish list',
+          color: AppColors.primaryGreenColor,
+          onDismissed: () {
+            if (overlayEntry != null) {
+              overlayEntry!.remove();
+              overlayEntry = null; // Clear the reference if needed
+            }
+          },
+        ),
+      ),
+    );
+    Overlay.of(context)?.insert(overlayEntry!);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: AlignmentDirectional.topEnd,
       child: Padding(
-        padding:  EdgeInsets.only(right: context.width*0.045,top:context.height*0.010 ),
+        padding: EdgeInsets.only(right: context.width * 0.045, top: context.height * 0.010),
         child: Column(
           children: [
-            const WhiteBoxIcon(icon: AppAssets.favourOutlinedIcon,),
-            VerticalSpace(context.height*0.012),
-            const WhiteBoxIcon(icon: AppAssets.compareIcon,),
+            WhiteBoxIcon(
+              icon: AppAssets.favourOutlinedIcon,
+              onTap: () => _showOverlay(context),
+            ),
+            VerticalSpace(context.height * 0.012),
+            WhiteBoxIcon(
+              icon: AppAssets.compareIcon,
+              onTap: () {}, // Define the onTap action for the compare icon if needed
+            ),
           ],
         ),
       ),
