d80f55bf7f16669a603fe735e51e90cb9918fa7d
20192726 <20192726@fue.edu.eg>
2024-02-12 14:38:06 +0200

    Cart Screen Address

--- lib/features/cart_screens/presentation/pages/cart_screen.dart
+++ lib/features/cart_screens/presentation/pages/cart_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:mega_top_mobile/features/cart_screens/presentation/widgets/empty_cart_page.dart';
+import 'package:mega_top_mobile/core/utils/app_assets.dart';
+import '../../../../core/utils/app_color.dart';
 import '../../../../core/utils/app_string.dart';
+import '../../../../core/utils/spacer.dart';
 import '../../../../core/widgets/app_bar_fav_icon.dart';
+import '../widgets/cart_screen_address.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -10,12 +15,16 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.circleAvatarBackground,
       appBar: PreferredSize(
           preferredSize: Size(double.infinity, context.height*0.089),
           child: const CustomFavouriteAppBar(AppStrings.shoppingCartEn)),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: context.width*0.045),
-        child: const EmptyCartPage(),
+      body: Column(
+        children: [
+          VerticalSpace(context.height*0.028),
+          const CartScreenAddress(),
+          VerticalSpace(context.height*0.028),
+        ],
       ),
     );
   }
