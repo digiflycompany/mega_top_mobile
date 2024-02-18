d9e13e06dd202c9cf87b162b018996bc446845bd
20192726 <20192726@fue.edu.eg>
2024-02-18 11:49:04 +0200

    CVV Text Field Reusable

--- lib/features/cart_screens/presentation/widgets/add_new_card_bottom_sheet.dart
+++ lib/features/cart_screens/presentation/widgets/add_new_card_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/features/categories_screens/presentation/widgets/bottom_sheet_app_bar.dart';
+import '../../../../core/widgets/primary_button.dart';
 import 'add_new_card_bottom_sheet_body.dart';
 
 class AddNewCardBottomSheet extends StatelessWidget {
@@ -12,13 +13,23 @@ class AddNewCardBottomSheet extends StatelessWidget {
     return Container(
       height: context.height*0.572,
       color: Colors.white,
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          BottomSheetAppBar(
+          const BottomSheetAppBar(
               AppStrings.addNewCardEn
           ),
-          AddNewCardBottomSheetBody()
+          const AddNewCardBottomSheetBody(),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: context.width*0.045),
+                child: const PrimaryButton(
+                  text: AppStrings.addBankCardEn,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
