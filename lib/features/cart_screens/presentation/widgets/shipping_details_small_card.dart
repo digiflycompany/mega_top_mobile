af52de6ef21f2c21756c26d92e4d213f6e0dde36
20192726 <20192726@fue.edu.eg>
2024-02-19 15:45:35 +0200

    Shipping Details Small Card

--- lib/features/cart_screens/presentation/widgets/shipping_details_small_card.dart
+++ lib/features/cart_screens/presentation/widgets/shipping_details_small_card.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/features/cart_screens/presentation/widgets/shipping_details_small_description.dart';
 import '../../../../core/utils/app_color.dart';
-import 'order_details_description.dart';
 import 'card_title_text.dart';
 
-class OrderDetailsCard extends StatelessWidget {
-  const OrderDetailsCard({super.key});
+class ShippingDetailsSmallCard extends StatelessWidget {
+  const ShippingDetailsSmallCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class OrderDetailsCard extends StatelessWidget {
       padding: EdgeInsets.only(top:context.height*0.033 ),
       child: Container(
         width: double.infinity,
-        height: context.height*0.284,
+        height: context.height*0.2235,
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border.all(width: 1.w,color: AppColors.smallContainerGreyColor),
@@ -26,9 +26,9 @@ class OrderDetailsCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const CardTitleText(title: AppStrings.orderDetailsEn,),
+              const CardTitleText(title: AppStrings.shippingDetailsEn,),
               Divider(color:AppColors.smallContainerGreyColor,thickness: 1.w,),
-              const OrderDetailsDescription(),
+              const ShippingDetailsSmallDescription()
             ],
           ),
         ),
---
+++ lib/features/cart_screens/presentation/widgets/shipping_details_small_description.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:mega_top_mobile/core/utils/app_assets.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/core/utils/spacer.dart';
+import 'package:mega_top_mobile/features/cart_screens/presentation/widgets/shipping_details_small_card_address.dart';
+
+class ShippingDetailsSmallDescription extends StatelessWidget {
+  const ShippingDetailsSmallDescription({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(right: context.width*0.045,left: context.width*0.045),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /// Location Icon
+                  SvgPicture.asset(AppAssets.locationOutlinedBlueIcon),
+                  HorizontalSpace(context.width*0.022),
+                  const ShippingDetailsSmallCardAddress(
+                    deliveryPlace: AppStrings.homeEn,
+                    address: AppStrings.addressNasrCityEn,
+                    phone: AppStrings.telephoneAddressEn,
+                  )
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
