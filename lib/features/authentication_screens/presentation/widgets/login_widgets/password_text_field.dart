4857976a5d30a756e56a86d3dfda81532f6818ba
20192726 <20192726@fue.edu.eg>
2024-03-17 14:29:00 +0200

    Password Text Field

---
+++ lib/features/authentication_screens/presentation/widgets/login_widgets/password_text_field.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mega_top_mobile/core/utils/app_assets.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/core/utils/extensions.dart';
+import 'package:mega_top_mobile/core/widgets/password_text_field.dart';
+
+class LoginPasswordTextField extends StatelessWidget {
+  const LoginPasswordTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: context.height24),
+      child: const PasswordTextField(
+        hintText: AppStrings.passwordEn,
+        prefixSvg: AppAssets.passwordIcon,
+      ),
+    );
+  }
+}
