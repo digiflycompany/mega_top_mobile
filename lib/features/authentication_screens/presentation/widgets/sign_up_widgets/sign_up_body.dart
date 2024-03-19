3d9ebfd607b7fb4d7d3338869497cc58c1c4db4e
20192726 <20192726@fue.edu.eg>
2024-03-19 11:31:51 +0200

    Sign Up Confirm Password Text Field

--- lib/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_body.dart
+++ lib/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_body.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mega_top_mobile/core/utils/app_assets.dart';
 import 'package:mega_top_mobile/core/utils/app_routes.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/core/utils/extensions.dart';
 import 'package:mega_top_mobile/core/utils/spacer.dart';
-import 'package:mega_top_mobile/core/widgets/password_text_field.dart';
 import 'package:mega_top_mobile/core/widgets/primary_button.dart';
 import 'package:mega_top_mobile/core/widgets/row_two_text.dart';
+import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_confirm_password_text_field.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_email_text_field.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_password_text_field.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_user_name_text_field.dart';
@@ -24,11 +23,7 @@ class SignUpBody extends StatelessWidget {
           const SignUpUserNameTextField(),
           const SignUpEmailTextField(),
           const SignUpPasswordTextField(),
-          const PasswordTextField(
-            hintText: AppStrings.confirmPasswordEn,
-            prefixSvg: AppAssets.passwordIcon,
-          ),
-          VerticalSpace(context.height * 0.055),
+          const SignUpConfirmPasswordTextField(),
           PrimaryButton(
             onTap: () {
               Routes.homePageRoute
