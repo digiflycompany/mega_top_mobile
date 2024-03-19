94c91467b0b97d62df1476cd4ecd7e46ee5d7bb4
20192726 <20192726@fue.edu.eg>
2024-03-19 11:41:43 +0200

    Already Have An Account Text

---
+++ lib/features/authentication_screens/presentation/widgets/sign_up_widgets/already_have_an_account.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mega_top_mobile/core/utils/app_routes.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/core/utils/extensions.dart';
+import 'package:mega_top_mobile/core/widgets/row_two_text.dart';
+
+class AlreadyHaveAnAccount extends StatelessWidget {
+  const AlreadyHaveAnAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RowTextButton(
+      firstText: AppStrings.alreadyHaveAnAccountEn,
+      buttonText: AppStrings.loginEn,
+      onTap: () {
+        Routes.loginRoute.moveTo;
+      },
+    );
+  }
+}
--- lib/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_body.dart
+++ lib/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_body.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mega_top_mobile/core/utils/app_routes.dart';
-import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/core/utils/extensions.dart';
-import 'package:mega_top_mobile/core/utils/spacer.dart';
-import 'package:mega_top_mobile/core/widgets/primary_button.dart';
-import 'package:mega_top_mobile/core/widgets/row_two_text.dart';
+import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/already_have_an_account.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_button.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_confirm_password_text_field.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/sign_up_email_text_field.dart';
@@ -26,13 +22,7 @@ class SignUpBody extends StatelessWidget {
           const SignUpPasswordTextField(),
           const SignUpConfirmPasswordTextField(),
           const SignUpButton(),
-          RowTextButton(
-            firstText: AppStrings.alreadyHaveAnAccountEn,
-            buttonText: AppStrings.loginEn,
-            onTap: () {
-              Routes.loginRoute.moveTo;
-            },
-          ),
+          const AlreadyHaveAnAccount(),
         ],
       ),
     );
