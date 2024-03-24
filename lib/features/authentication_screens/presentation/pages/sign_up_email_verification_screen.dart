1a428bfd283bdcb519be978278124cd23c8550ff
20192726 <20192726@fue.edu.eg>
2024-03-24 12:06:41 +0200

    User Email Text

--- lib/features/authentication_screens/presentation/pages/sign_up_email_verification_screen.dart
+++ lib/features/authentication_screens/presentation/pages/sign_up_email_verification_screen.dart
@@ -3,6 +3,7 @@ import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/core/utils/extensions.dart';
 import 'package:mega_top_mobile/core/widgets/custom_app_bar.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/please_enter_four_digits_code_text.dart';
+import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/sign_up_widgets/user_email_text.dart';
 
 class SignUpEmailVerificationScreen extends StatelessWidget {
   const SignUpEmailVerificationScreen({super.key});
@@ -16,9 +17,10 @@ class SignUpEmailVerificationScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: context.width16),
         child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
            children: [
              PleaseEnterFourDigitsCodeText(),
-
+             UserEmailText(),
            ],
         ),
       ),
---
+++ lib/features/authentication_screens/presentation/widgets/sign_up_widgets/user_email_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mega_top_mobile/core/utils/app_string.dart';
+import 'package:mega_top_mobile/core/utils/extensions.dart';
+
+class UserEmailText extends StatelessWidget {
+  const UserEmailText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: context.height6),
+      child: Text(
+        AppStrings.userEmail,
+        style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 14.sp
+        ),
+      ),
+    );
+  }
+}
