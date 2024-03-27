0492a81acc1e2e5c8cb1990a348fb4831edfbab5
20192726 <20192726@fue.edu.eg>
2024-03-27 11:39:31 +0200

    Reset Password Using Email Done

--- lib/features/authentication_screens/presentation/pages/reset_password_screen.dart
+++ lib/features/authentication_screens/presentation/pages/reset_password_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mega_top_mobile/core/utils/app_routes.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/core/utils/extensions.dart';
 import 'package:mega_top_mobile/core/widgets/custom_app_bar.dart';
+import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_cubit.dart';
+import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_state.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/reset_password_widgets/reset_password_button.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/reset_password_widgets/reset_password_condition.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/reset_password_widgets/reset_password_description.dart';
@@ -12,23 +16,36 @@ class ResetPasswordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size(double.infinity, context.height * 0.089),
-          child: const CustomAppBar(AppStrings.resetPasswordEn)),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: context.width * 0.045),
-        child: SingleChildScrollView(
-          child: const Column(
-            children: [
-               ResetPasswordCondition(),
-               ResetPasswordDescription(),
-               ResetPasswordEmailField(),
-               ResetPasswordButton(),
-            ],
+    final _formKey = GlobalKey<FormState>();
+    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
+      listener: (context, state) {
+        if(state is ResetPasswordSuccess){
+           Routes.verifyEmailRoute.moveTo;
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size(double.infinity, context.height * 0.089),
+              child: const CustomAppBar(AppStrings.resetPasswordEn)),
+          body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.width16),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const ResetPasswordCondition(),
+                    const ResetPasswordDescription(),
+                    const ResetPasswordEmailField(),
+                    ResetPasswordButton(formKey: _formKey,),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/features/authentication_screens/presentation/widgets/reset_password_widgets/reset_password_button.dart
+++ lib/features/authentication_screens/presentation/widgets/reset_password_widgets/reset_password_button.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mega_top_mobile/core/utils/app_routes.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
-import 'package:mega_top_mobile/core/utils/extensions.dart';
 import 'package:mega_top_mobile/core/widgets/auth_button.dart';
+import 'package:mega_top_mobile/core/widgets/button_circular_progress.dart';
 import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_cubit.dart';
 import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_state.dart';
 
 class ResetPasswordButton extends StatelessWidget {
-  const ResetPasswordButton({super.key});
+  final GlobalKey<FormState> formKey;
+  const ResetPasswordButton({super.key, required this.formKey});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AuthenticationCubit, AuthenticationState>(
       builder: (context, state) {
-        //AuthenticationCubit resetCubit = context.read<AuthenticationCubit>();
+        AuthenticationCubit resetCubit = context.read<AuthenticationCubit>();
         return AuthButton(
-          content: Text(
+          content: state is ResetPasswordLoading?const ButtonCircularProgress():Text(
             AppStrings.sendEn,
             style: TextStyle(
               color: Colors.white,
@@ -26,7 +26,9 @@ class ResetPasswordButton extends StatelessWidget {
             ),
           ),
           onTap: () {
-            Routes.verifyEmailRoute.moveTo;
+            if(formKey.currentState!.validate()){
+              resetCubit.resetPassword(resetCubit.resetPasswordEmailController.text);
+            }
           },
         );
       },
