3775aa9cc77fe0ed85617372a55c9029d74c7916
20192726 <20192726@fue.edu.eg>
2024-03-28 14:56:51 +0200

    Confirm New Password Text Field

--- lib/features/authentication_screens/presentation/pages/create_new_password_screen.dart
+++ lib/features/authentication_screens/presentation/pages/create_new_password_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mega_top_mobile/core/utils/app_assets.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/core/utils/extensions.dart';
-import 'package:mega_top_mobile/core/utils/spacer.dart';
 import 'package:mega_top_mobile/core/widgets/custom_app_bar.dart';
-import 'package:mega_top_mobile/core/widgets/password_text_field.dart';
 import 'package:mega_top_mobile/core/widgets/primary_button.dart';
 import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_cubit.dart';
 import 'package:mega_top_mobile/features/authentication_screens/cubit/auth_state.dart';
+import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/create_new_password_widgets/confirm_new_password_text_field.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/create_new_password_widgets/create_new_password_condition.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/create_new_password_widgets/create_new_password_description.dart';
 import 'package:mega_top_mobile/features/authentication_screens/presentation/widgets/create_new_password_widgets/create_new_password_text_field.dart';
@@ -31,15 +29,7 @@ class CreateNewPasswordScreen extends StatelessWidget {
               const CreateNewPasswordCondition(),
               const CreateNewPasswordDescription(),
               const CreateNewPasswordTextField(),
-              BlocBuilder<AuthenticationCubit, AuthenticationState>(
-                builder: (context, state) {
-                  return const PasswordTextField(
-                    hintText: AppStrings.confirmYourNewPasswordEn,
-                    prefixSvg: AppAssets.passwordIcon,
-                  );
-                },
-              ),
-              VerticalSpace(context.height * 0.055),
+              const ConfirmNewPasswordTextField(),
               BlocBuilder<AuthenticationCubit, AuthenticationState>(
                 builder: (context, state) {
                   AuthenticationCubit authenticationCubit = context.read<AuthenticationCubit>();
