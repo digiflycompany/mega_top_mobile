ab52d0165fe27c2094effab63da35d423fc3016d
20192726 <20192726@fue.edu.eg>
2024-08-25 14:03:49 +0300

    Navigating to the website

--- lib/features/account_screens/account_details_screen/presentation/widgets/call_us_item.dart
+++ lib/features/account_screens/account_details_screen/presentation/widgets/call_us_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mega_top_mobile/core/utils/app_assets.dart';
 import 'package:mega_top_mobile/core/utils/app_string.dart';
 import 'package:mega_top_mobile/features/account_screens/account_details_screen/presentation/widgets/account_option_item.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CallUsItem extends StatelessWidget {
   const CallUsItem({super.key, this.mainIcon, this.title, this.optionalData});
@@ -16,9 +17,21 @@ class CallUsItem extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(top: 25.h),
       child: AccountOptionItem(
+        onTap: () {
+          _launchURL('https://megatop.com.eg/en/contact');
+        },
         mainIcon: mainIcon ?? AppAssets.callIcon,
         title: title ?? AppStrings.callUs,
       ),
     );
+  }
+
+  void _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
