fe7192d43f90b26902ecfa6c07e2eee6682814da
20192726 <20192726@fue.edu.eg>
2024-09-17 17:30:39 +0300

    Home adds model and api

--- lib/features/home_screens/presentation/widgets/home_ads.dart
+++ lib/features/home_screens/presentation/widgets/home_ads.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mega_top_mobile/core/utils/app_color.dart';
 import 'package:mega_top_mobile/features/home_screens/presentation/widgets/ad_card.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mega_top_mobile/bloc_observer.dart';
 import 'package:mega_top_mobile/services/shared_preferences/preferences_helper.dart';
-
 import 'features/home_screens/presentation/pages/my_app.dart';
 
 void main() async {
@@ -15,6 +12,5 @@ void main() async {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Future.delayed(Duration(seconds: 1));
   PreferencesHelper.init();
-  runApp(DevicePreview(
-      enabled: !kReleaseMode, builder: (BuildContext context) => MyApp()));
+  runApp(MyApp());
 }
