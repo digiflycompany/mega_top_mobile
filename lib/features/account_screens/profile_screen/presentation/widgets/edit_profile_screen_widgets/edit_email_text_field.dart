8d1a9c3854003fdc3c6364eca950595a4250644a
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2024-09-30 12:11:17 +0300

    Edit Profile Email Validation Bug Resolved ✔

--- lib/features/account_screens/profile_screen/presentation/widgets/edit_profile_screen_widgets/edit_email_text_field.dart
+++ lib/features/account_screens/profile_screen/presentation/widgets/edit_profile_screen_widgets/edit_email_text_field.dart
@@ -7,30 +7,26 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class EditEmailTextField extends StatelessWidget {
   final String title;
   final TextEditingController controller;
-  const EditEmailTextField({super.key, required this.title, required this.controller});
+  const EditEmailTextField(
+      {super.key, required this.title, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    final emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$');
+    final emailRegex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
 
     return EditTextField(
-      title: title,
-      controller: controller,
-      validator: (value){
-        if (value == null || value.isEmpty) {
-          return AppLocalizations.of(context)!.pleaseEnterYourEmail;
-        } else if(emailRegex.hasMatch(value)){
-          return AppLocalizations.of(context)!.pleaseEnterValidEmailAddress;
-        }
-        return null;
-      },
-      suffix: Transform.scale(
-        scale: 0.5,
-        child: SvgPicture.asset(
-          AppAssets.editIcon,
-        ),
-
-      ),
-    );
+        title: title,
+        controller: controller,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return AppLocalizations.of(context)!.pleaseEnterYourEmail;
+          } else if (!emailRegex.hasMatch(value)) {
+            return AppLocalizations.of(context)!.pleaseEnterValidEmailAddress;
+          }
+          return null;
+        },
+        suffix: Transform.scale(
+            scale: 0.5, child: SvgPicture.asset(AppAssets.editIcon)));
   }
 }
