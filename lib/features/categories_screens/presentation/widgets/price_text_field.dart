4f8b69e149b284c4660aa08a84fd19c119ac4221
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2024-10-03 17:42:25 +0300

    Decimal Point Handled ✔

--- lib/features/categories_screens/presentation/widgets/price_text_field.dart
+++ lib/features/categories_screens/presentation/widgets/price_text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mega_top_mobile/core/utils/app_color.dart';
@@ -15,39 +14,66 @@ class PriceTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CategoryCubit, CategoryState>(builder: (context, state) {
-      return SizedBox(
+    return BlocBuilder<CategoryCubit, CategoryState>(
+      builder: (context, state) {
+        return SizedBox(
           width: context.width * 0.433,
           height: context.height * 0.065,
           child: TextFormField(
-              controller: price,
-              cursorColor: AppColors.blackGreyColor,
-              decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.priceInEGP,
-                  labelStyle: TextStyle(
-                      color: AppColors.blackGreyColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14.sp),
-                  border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(context.height * 0.006)),
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: context.width * 0.043,
-                      vertical: context.height * 0.015),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: AppColors.smallContainerGreyColor),
-                      borderRadius:
-                          BorderRadius.circular(context.height * 0.006)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: AppColors.smallContainerGreyColor),
-                      borderRadius:
-                          BorderRadius.circular(context.height * 0.006))),
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              keyboardType: TextInputType.number));
-    });
+            controller: price,
+            cursorColor: AppColors.blackGreyColor,
+            decoration: InputDecoration(
+              labelText: AppLocalizations.of(context)!.priceInEGP,
+              labelStyle: TextStyle(
+                color: AppColors.blackGreyColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 14.sp,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(context.height * 0.006),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: context.width * 0.043,
+                vertical: context.height * 0.015,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: AppColors.smallContainerGreyColor,
+                ),
+                borderRadius: BorderRadius.circular(context.height * 0.006),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: AppColors.smallContainerGreyColor,
+                ),
+                borderRadius: BorderRadius.circular(context.height * 0.006),
+              ),
+            ),
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a price';
+              }
+
+              // Check if the last character is an integer
+              final lastChar = value[value.length - 1];
+              if (int.tryParse(lastChar) != null) {
+                return 'The last character must not be a number';
+              }
+
+              // Check for multiple decimal points
+              if (value.split('.').length - 1 > 1) {
+                return 'Only one decimal point is allowed';
+              }
+
+              if (lastChar == '.') {
+                return 'The last character must not be a decimal point';
+              }
+              return null; // Valid
+            },
+          ),
+        );
+      },
+    );
   }
 }
