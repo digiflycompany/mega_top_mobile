8d61c8b42e2fc5998536ff4804ebbf010eec60db
20192726 <20192726@fue.edu.eg>
2024-12-17 11:12:25 +0200

    Removed Device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,10 +12,5 @@ void main() async {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Future.delayed(Duration(seconds: 1));
   PreferencesHelper.init();
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-    builder: (context) {
-      return MyApp();
-    }
-  ));
+  runApp(MyApp());
 }
